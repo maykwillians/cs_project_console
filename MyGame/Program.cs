98149795d6dd63d0778625d094bf6a2bc5e88749
mayk.menezes <mayk.menezes@objective.com.br>
2020-07-24 00:00:21 -0300

    initial commit

---
+++ MyGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
